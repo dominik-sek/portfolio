91f45daf68fba9a9020d0391fa8e09b68cecc9a4
Dominik Sęk <d.sek464@gmail.com>
2023-04-27 11:57:34 +0200

    fix prettier cfg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
-import type { NextPage } from 'next'
-
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div className={'w-full h-screen bg-black flex justify-center items-center text-white'}>
-      <div className={'font-sans font-bold'} >
-        inter
-      </div>
-      <div>
-
-      </div>
+      <div className={'font-sans font-bold'}>inter</div>
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
