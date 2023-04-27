ac86546dd977642d78a50ef00fdc8bf1c4802967
Dominik Sęk <d.sek464@gmail.com>
2023-04-27 11:57:19 +0200

    add husky, tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,13 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={'w-full h-screen bg-black flex justify-center items-center text-white'}>
+      <div className={'font-sans font-bold'} >
+        inter
+      </div>
+      <div>
 
-
+      </div>
     </div>
   )
 }
